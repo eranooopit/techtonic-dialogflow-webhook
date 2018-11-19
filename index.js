12babab4d928c6aecfa585304f1d0072ed538d74
eranooopit <45172539+eranooopit@users.noreply.github.com>
2018-11-19 19:55:52 +0530

    Adding webhook code to index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/coverage", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.client
+      ? req.body.result.parameters.client
+      : "Seems like some problem. Please Try again..";
+  return res.json({
+    fulfillmentText: speech,
+    fulfillmentMessages: [
+      {
+        text: {
+          text: [
+            speech
+          ]
+        }
+      },
+    source: "techtonic-dialogflow-webhook"
+  });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("***** Server up and listening *****");
+});
