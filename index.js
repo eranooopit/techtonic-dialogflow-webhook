4d0e6d1e313535174b572c45f3fefee18ceb78c3
eranooopit <45172539+eranooopit@users.noreply.github.com>
2018-11-20 00:25:23 +0530

    multiple parameter test

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/coverage", function(req, res) {
+  console.log('Parameter List - ' + req.body.result.parameters);
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
